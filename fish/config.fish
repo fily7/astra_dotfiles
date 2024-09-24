4930a80e64fdd3967373289cfac7f44a0953a967
Игорь Алексеевич Поташев <ipotashev@niac-ipotashev.ald.adm-nao.ru>
2024-09-24 12:28:38 +0300

    clean old

--- fish/config.fish
+++ fish/config.fish
@@ -14,8 +14,6 @@ set NVM_DIR $HOME/.nvm
 set GOROOT /usr/local/go
 set GOPATH $HOME/DEV/go
 
-set USER_NAME "ipotashev"
-set USER_EMAIL "fil_igor@bk.ru"
 
 alias cat bat
 
@@ -39,16 +37,3 @@ alias dup "docker-compose up -d"
 alias dstop "docker-compose stop"
 alias ddown "docker-compose down"
 alias devs "sh scripts/copy_ssh.sh && docker-compose up -d && docker exec -it \$(basename \"\$PWD\")_station_1 tmux"
-
-#alias updatedev "ssh devbot './update_dev.sh'"
-#alias love "python3 $HOME/scripts/lovegen.py 10 | xclip -selection clipboard"
-#alias big_love "python3 $HOME/scripts/lovegen.py 100 | xclip -selection clipboard"
-
-#if test -e $NVM_DIR/nvm.sh
-#    sh $NVM_DIR/nvm.sh
-#end
-#nvm use lts --silent
-if test -d $FNM_PATH
-   fish_add_path $FNM_PATH
-   fnm env | source
-end
